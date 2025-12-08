7dd4552edc900dc1598807a8b1785d533ce64d7f
avin <avinashsomwaru06@gmail.com>
2025-12-08 14:12:10 -0500

    init

---
+++ src/main/java/net/avin2810/cobblemonhealingbackpacks/CobblemonHealingBackpacksDataGenerator.java
@@ -0,0 +1,11 @@
+package net.avin2810.cobblemonhealingbackpacks;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CobblemonHealingBackpacksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/avin2810/cobblemonhealingbackpacks/ModClient.java
@@ -0,0 +1,10 @@
+package net.avin2810.cobblemonhealingbackpacks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
